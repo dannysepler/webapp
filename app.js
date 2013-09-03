2c8bbbb21aca5d03f087f311e32575aacc17fc71
Danny Sepler <dannysepler@gmail.com>
2013-09-02 22:24:32 -0400

    port fix

--- app.js
+++ app.js
@@ -38,10 +38,12 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+//http.createServer(app).listen(app.get('port'), function(){
+  //console.log('Express server listening on port ' + app.get('port'));
+//});
+app.listen(port, function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
-
 
 
 
