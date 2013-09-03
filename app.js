cf2a17a07d3e74bc9f99b386c91533548bb336b5
Danny Sepler <dannysepler@gmail.com>
2013-09-03 15:48:54 -0400

    revert back

--- app.js
+++ app.js
@@ -41,9 +41,6 @@ http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-
-
-
 app.get('/', function(req, res){
   res.render('index', {
     title: 'Home'
