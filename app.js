7df9fefb698fc047b12f9591ac3717bcf68c351c
Danny Sepler <dannysepler@gmail.com>
2013-09-03 10:09:36 -0400

    got rid of connect

--- app.js
+++ app.js
@@ -8,9 +8,8 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-var connect = require('connect');
+//var connect = require('connect');
 var request = require('request');
-var port = process.env.PORT || 3000;
 //var sys = require('sys');
 //var s = connect.createServer();
 
@@ -38,10 +37,7 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-//http.createServer(app).listen(app.get('port'), function(){
-  //console.log('Express server listening on port ' + app.get('port'));
-//});
-app.listen(port, function() {
+http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
